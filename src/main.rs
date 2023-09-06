7f2f5c2cf9fedf30402a8f8998afd9ff851e63c5
Stephan D Ramalho <sramalho@fordham.edu>
2023-09-06 13:45:08 -0400

    refactored main to user handler_hello function

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,24 @@
 
 use std::net::SocketAddr;
 
-use axum::{Router, routing::get, response::Html};
+use axum::{Router, routing::get, response::{Html, IntoResponse}};
 
 #[tokio::main]
 async fn main() {
-    let routes_hello = Router::new().route("/hello", get(|| async {Html("Hello <strong>World!!!</strong>")}),);
+    let routes_hello: Router = Router::new().route("/hello", get(handler_hello));
 
-    // region: --- Start Server
+    // region -- Start Server
     let addr = SocketAddr::from(([127,0,0,1], 8080));
-    println!("->> LISTENING on {}\n", addr);
+    println!("->> LISTENING on {addr}\n");
     axum::Server::bind(&addr)
         .serve(routes_hello.into_make_service())
         .await
-        .unwrap();
-        
-    // endregion: --- Start Server
+        .unwrap()
+}
+
+// region: -- Handler Hello
+async fn handler_hello() -> impl IntoResponse {
+    println!("--> {:<12} - handler_hello", "HANDLER");
+
+    Html("Hello <strong>World!!!</strong>")
 }
