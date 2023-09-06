47814efbf0a043117e77d09d8e248fa8cccc3c99
Stephan D Ramalho <sramalho@fordham.edu>
2023-09-06 09:58:49 -0400

    created hello world route

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,19 @@
 #![allow(unused)]
 
+use std::net::SocketAddr;
+
+use axum::{Router, routing::get, response::Html};
+
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
+    let routes_hello = Router::new().route("/hello", get(|| async {Html("Hello <strong>World!!!</strong>")}),);
+
+    // region: --- Start Server
+    let addr = SocketAddr::from(([127,0,0,1], 8080));
+    axum::Server::bind(&addr)
+        .serve(routes_hello.into_make_service())
+        .await
+        .unwrap();
+    println!("->> LISTENING on {addr}\n");
+    // endregion: --- Start Server
 }
