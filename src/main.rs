b1bff8d5e154ba0b1678c1fc90c656f74ace5b27
Stephan D Ramalho <sramalho@fordham.edu>
2023-09-07 15:29:09 -0400

    created hello2 route and modified test to confirm it works properly

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,14 @@
 
 use std::net::SocketAddr;
 
-use axum::{Router, routing::get, response::{Html, IntoResponse}, extract::Query};
+use axum::{Router, routing::get, response::{Html, IntoResponse}, extract::{Query, Path}};
 use serde::Deserialize;
 
 #[tokio::main]
 async fn main() {
-    let routes_hello: Router = Router::new().route("/hello", get(handler_hello));
+    let routes_hello: Router = Router::new()
+    .route("/hello", get(handler_hello))
+    .route("/hello2/:name", get(handler_hello2));
 
     // region -- Start Server
     let addr = SocketAddr::from(([127,0,0,1], 8080));
@@ -26,11 +28,18 @@ struct HelloParams {
     name: Option<String>,
 
 }
-
+// ex. `/hello?name=Trundle`
 async fn handler_hello(Query(params): Query<HelloParams>) -> impl IntoResponse {
     println!("--> {:<12} - handler_hello - {params:?}", "HANDLER");
 
     let name = params.name.as_deref().unwrap_or("World!");
     Html(format!("Hello <strong>{name}</strong>"))
 }
+
+// ex. `hello2/Mike`
+async fn handler_hello2(Path(name): Path<String>) -> impl IntoResponse{
+    print!("->> {:<12} - hander_hello2 - {name:?}", "HANDLER");
+    Html(format!("Hello2 <strong>{name}</strong>"))
+}
+
 // endregion: -- Handler Hello
--- tests/quick_dev.rs
+++ tests/quick_dev.rs
@@ -6,7 +6,7 @@ use anyhow::{Result, Ok};
 async fn quick_dev() -> Result<()> {
   let hc = httpc_test::new_client("http://localhost:8080")?;
 
-  hc.do_get("/hello?name=Trundle")
+  hc.do_get("/hello2/slagsloth")
     .await?
     .print()
     .await?;
